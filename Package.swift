33a44a95ec5363e3385204aad512f57e70bc2955
Jonathan Guthrie <jono@perfect.org>
2016-12-07 21:36:55 -0500

    Adding logging to package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
 	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 0, minor: 0)
+		.Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 0, minor: 0),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
 	],
 	exclude: []
 )
