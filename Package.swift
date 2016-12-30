78cc96b9c2e18c1761a22089df3a9e77f14269ce
Jonathan Guthrie <jono@perfect.org>
2016-12-30 16:07:53 -0500

    update deps versions

--- Package.swift
+++ Package.swift
@@ -1,20 +1,12 @@
-//
-//  Package.swift
-//  Postgres StORM
-//
-//  Created by Jonathan Guthrie on 2016-09-23.
-//	Copyright (C) 2016 Jonathan Guthrie.
-//
-
+// Generated automatically by Perfect Assistant Application
+// Date: 2016-12-30 21:07:16 +0000
 import PackageDescription
-
 let package = Package(
-	name: "MySQLStORM",
-	targets: [],
-	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 0, minor: 0),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
-	],
-	exclude: []
+    name: "MySQLStORM",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 1),
+        .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
+    ]
 )
