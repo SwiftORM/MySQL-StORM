3650f9b79bb639fc20e4bfd66cdf612c168b9242
Jonathan Guthrie <jono@perfect.org>
2017-01-12 17:16:39 -0500

    update logger rev

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-30 21:07:16 +0000
+// Date: 2017-01-12 22:15:25 +0000
 import PackageDescription
 let package = Package(
     name: "MySQLStORM",
@@ -7,6 +7,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 1),
         .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1, minor: 0),
     ]
 )
