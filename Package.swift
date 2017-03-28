ec500a237281e3904c85109845ef0b3c4eeccdf3
Jonathan Guthrie <jono@perfect.org>
2017-03-28 11:07:23 -0400

    update to use major version only

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ let package = Package(
     name: "MySQLStORM",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 1),
-        .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2),
+        .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
     ]
 )
