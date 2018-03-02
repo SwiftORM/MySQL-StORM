f3872ff9953e98151a7c883800aa3f95b8e0e88d
Jonathan Guthrie <jono@guthrie.net.nz>
2018-03-02 11:22:53 -0500

    Adding socket support

--- Sources/MySQLConnect.swift
+++ Sources/MySQLConnect.swift
@@ -32,32 +32,48 @@ open class MySQLConnect: StORMConnect {
         password: String = "",
         database: String = "",
         port: Int = 0,
-        charset: String = "utf8mb4") {
+        charset: String = "utf8mb4",
+		method: StORMConnectionMethod = .network) {
         super.init()
         self.database = database
         self.datasource = .MySQL
         let _ = self.server.setOption(.MYSQL_SET_CHARSET_NAME, charset)
         self.charset = charset
-        self.credentials = StORMDataSourceCredentials(host: host, port: port, user: username, pass: password)
+		self.credentials = StORMDataSourceCredentials(host: host, port: port, user: username, pass: password, method: method)
     }
     
     /// Opens the connection
     /// If an error is generated, the connection state will be output to console and to ./StORMlog.txt
     public func open() {
-        let status = server.connect(
-            host: self.credentials.host,
-            user: self.credentials.username,
-            password: self.credentials.password,
-            db: self.database,
-            port: UInt32(self.credentials.port)
-        )
-        
-        guard status else {
-            // verify connection success
-            LogFile.error("MySQL connection error: \(server.errorMessage())", logFile: "./StORMlog.txt")
-            resultCode = .error(server.errorMessage())
-            return
-        }
+		if self.credentials.method == .socket {
+			let status = server.connect(
+				user: self.credentials.username,
+				password: self.credentials.password,
+				db: self.database,
+				socket: self.credentials.host)
+
+			guard status else {
+				// verify connection success
+				LogFile.error("MySQL network connection error: \(server.errorMessage())", logFile: "./StORMlog.txt")
+				resultCode = .error(server.errorMessage())
+				return
+			}
+		} else {
+			let status = server.connect(
+				host: self.credentials.host,
+				user: self.credentials.username,
+				password: self.credentials.password,
+				db: self.database,
+				port: UInt32(self.credentials.port)
+			)
+
+			guard status else {
+				// verify connection success
+				LogFile.error("MySQL socket connection error: \(server.errorMessage())", logFile: "./StORMlog.txt")
+				resultCode = .error(server.errorMessage())
+				return
+			}
+		}
         resultCode = .noError
     }
     
