b930234f56011b0573f11de4dadefa24a55f3a8e
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-03-26 13:19:27 -0400

    Updated package file

--- Package.swift
+++ Package.swift
@@ -1,12 +1,18 @@
+// swift-tools-version:4.2
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-10-03 17:18:40 +0000
 import PackageDescription
 let package = Package(
 	name: "MySQLStORM",
-	targets: [],
+	products: [
+		.library(name: "MySQLStORM", targets: ["MySQLStORM"])
+	],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 3),
-		.Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 3),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", from: "3.0.0"),
+		.package(url: "https://github.com/SwiftORM/StORM.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
+	],
+	targets: [
+		.target(name: "MySQLStORM", dependencies: ["PerfectMySQL", "StORM", "PerfectLogger"])
 	]
 )
